25c297f6838cffea0c552f8c89b46d92d4477212
Ubuntu <ubuntu@ip-172-31-22-87.us-east-2.compute.internal>
2020-06-30 13:53:40 +0000

    Update files for submission

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=thrld/udac_project_5
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-service --image=$dockerpath --port=80 --labels="app=ml-service"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+POD_NAME=$(kubectl get pods -l app=ml-service -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward $POD_NAME 8000:80
 
